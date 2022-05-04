0bd921f2e32665bd921732f9aa5991aa4ed5df7a
Hayato Yagi <hayatoy1013@gmail.com>
2022-05-05 01:03:24 +0900

    Implement notification

--- lib/ui/settings/settings_page.dart
+++ lib/ui/settings/settings_page.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:health_check/ui/edit_profile/edit_profile_page.dart';
 import 'package:health_check/ui/launch/launch_page.dart';
 import 'package:settings_ui/settings_ui.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 // class SettingsPage extends StatelessWidget {
 //   const SettingsPage({Key? key}) : super(key: key);
@@ -101,6 +105,9 @@ class _SettingsPage extends State<SettingsPage> {
   final _notificationHourKey = "notificationHour";
   final _notificationMinuteKey = "notificationMinute";
 
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
   bool _settingsLoaded = false;
   bool _enableNotification = false;
   TimeOfDay _notificationTime = const TimeOfDay(hour: 8, minute: 0);
@@ -111,6 +118,8 @@ class _SettingsPage extends State<SettingsPage> {
     });
     final prefs = await SharedPreferences.getInstance();
     prefs.setBool(_enableNotificationKey, value);
+
+    _setNotification();
   }
 
   void _setNotificationTime(TimeOfDay value) async {
@@ -120,15 +129,73 @@ class _SettingsPage extends State<SettingsPage> {
     final prefs = await SharedPreferences.getInstance();
     prefs.setInt(_notificationHourKey, value.hour);
     prefs.setInt(_notificationMinuteKey, value.minute);
+    _setNotification();
   }
 
   @override
   void initState() {
     super.initState();
-    loadSettings();
+    _loadSettings();
+    _configureLocalTimeZone();
+    _initNotification();
   }
 
-  void loadSettings() async {
+  Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName!));
+  }
+
+  void _initNotification() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('icon512');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  // todo: アプリをキルすると通知が来ないっぽい？
+  Future<void> _setNotification() async {
+    await _flutterLocalNotificationsPlugin.cancelAll();
+
+    if (!_enableNotification) return;
+
+    final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+    tz.TZDateTime scheduledDate = tz.TZDateTime(
+      tz.local,
+      now.year,
+      now.month,
+      now.day,
+      _notificationTime.hour,
+      _notificationTime.minute,
+    );
+
+    await _flutterLocalNotificationsPlugin.zonedSchedule(
+      0,
+      'Health Check',
+      '健康観察フォームを提出してください',
+      scheduledDate,
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'reminder',
+          'リマインダー',
+          importance: Importance.max,
+          priority: Priority.high,
+          // styleInformation: BigTextStyleInformation('健康観察フォームを提出してください'),
+          // icon: 'icon512',
+        ),
+      ),
+      androidAllowWhileIdle: true,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.time,
+    );
+  }
+
+  void _loadSettings() async {
     final prefs = await SharedPreferences.getInstance();
     setState(() {
       _enableNotification = prefs.getBool(_enableNotificationKey) ?? false;
@@ -149,62 +216,62 @@ class _SettingsPage extends State<SettingsPage> {
       ),
       body: _settingsLoaded
           ? SettingsList(
-        sections: [
-          SettingsSection(
-            title: const Text("通知設定"),
-            tiles: [
-              SettingsTile.switchTile(
-                initialValue: _enableNotification,
-                onToggle: _setEnableNotification,
-                title: const Text("通知オン"),
-              ),
-              SettingsTile(
-                title: const Text("通知時刻"),
-                trailing: Text(
-                    '${_notificationTime.hour.toString().padLeft(2, '0')}:${_notificationTime.minute.toString().padLeft(2, '0')}'),
-                onPressed: (BuildContext context) async {
-                  TimeOfDay? value = await showTimePicker(
-                    context: context,
-                    initialTime: _notificationTime,
-                  );
-                  if (value != null) {
-                    _setNotificationTime(value);
-                  }
-                },
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: const Text("アカウント設定"),
-            tiles: [
-              SettingsTile.navigation(
-                title: const Text("登録情報変更"),
-                onPressed: (BuildContext context) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const EditProfilePage()),
-                  );
-                },
-              ),
-              SettingsTile.navigation(
-                leading: const Icon(Icons.logout),
-                title: const Text("ログアウト"),
-                onPressed: (BuildContext context) {
+              sections: [
+                SettingsSection(
+                  title: const Text("通知設定"),
+                  tiles: [
+                    SettingsTile.switchTile(
+                      initialValue: _enableNotification,
+                      onToggle: _setEnableNotification,
+                      title: const Text("通知オン"),
+                    ),
+                    SettingsTile(
+                      title: const Text("通知時刻"),
+                      trailing: Text(
+                          '${_notificationTime.hour.toString().padLeft(2, '0')}:${_notificationTime.minute.toString().padLeft(2, '0')}'),
+                      onPressed: (BuildContext context) async {
+                        TimeOfDay? value = await showTimePicker(
+                          context: context,
+                          initialTime: _notificationTime,
+                        );
+                        if (value != null) {
+                          _setNotificationTime(value);
+                        }
+                      },
+                    ),
+                  ],
+                ),
+                SettingsSection(
+                  title: const Text("アカウント設定"),
+                  tiles: [
+                    SettingsTile.navigation(
+                      title: const Text("登録情報変更"),
+                      onPressed: (BuildContext context) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const EditProfilePage()),
+                        );
+                      },
+                    ),
+                    SettingsTile.navigation(
+                      leading: const Icon(Icons.logout),
+                      title: const Text("ログアウト"),
+                      onPressed: (BuildContext context) {
                         FirebaseAuth.instance.signOut();
                         Navigator.pushReplacement(
                             context,
                             MaterialPageRoute(
                                 builder: (context) => const LaunchPage()));
                       },
-              ),
-            ],
-          ),
-        ],
-      )
+                    ),
+                  ],
+                ),
+              ],
+            )
           : const Center(
-        child: CircularProgressIndicator(),
-      ),
+              child: CircularProgressIndicator(),
+            ),
     );
   }
 }
--- lib/ui/settings/settings_view_model.dart
+++ lib/ui/settings/settings_view_model.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 /// このファイルは現在使っていない
 /// settings_uiパッケージがproviderを使うと上手く動かない（notifyChangesで画面が更新されない）ので、StatefulWidgetで実装した
@@ -7,6 +11,9 @@ class SettingsViewModel extends ChangeNotifier {
   final _enableNotificationKey = "enableNotification";
   final _notificationHourKey = "notificationHour";
   final _notificationMinuteKey = "notificationMinute";
+
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   bool? _enableNotification;
 
@@ -16,6 +23,42 @@ class SettingsViewModel extends ChangeNotifier {
     final prefs = await SharedPreferences.getInstance();
     prefs.setBool(_enableNotificationKey, value);
     notifyListeners();
+
+    if (value) {
+      await _flutterLocalNotificationsPlugin.cancelAll();
+      final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
+      tz.TZDateTime scheduledDate = tz.TZDateTime(
+        tz.local,
+        now.year,
+        now.month,
+        now.day,
+        notificationTime.hour,
+        notificationTime.minute,
+      );
+
+      await _flutterLocalNotificationsPlugin.zonedSchedule(
+        0,
+        'flutter_local_notifications',
+        'message',
+        scheduledDate,
+        const NotificationDetails(
+          android: AndroidNotificationDetails(
+            'channel id',
+            'channel name',
+            importance: Importance.max,
+            priority: Priority.high,
+            ongoing: true,
+            styleInformation: BigTextStyleInformation('message'),
+            icon: 'ic_notification',
+          ),
+        ),
+        androidAllowWhileIdle: true,
+        uiLocalNotificationDateInterpretation:
+            UILocalNotificationDateInterpretation.absoluteTime,
+        matchDateTimeComponents: DateTimeComponents.time,
+      );
+      print('set notification');
+    }
   }
 
   TimeOfDay? _notificationTime;
@@ -38,5 +81,25 @@ class SettingsViewModel extends ChangeNotifier {
       minute: prefs.getInt(_notificationMinuteKey) ?? 0,
     );
     notifyListeners();
+
+    _configureLocalTimeZone();
+    _initNotification();
+  }
+
+  Future<void> _configureLocalTimeZone() async {
+    tz.initializeTimeZones();
+    final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+    tz.setLocalLocation(tz.getLocation(timeZoneName!));
+  }
+
+  void _initNotification() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('ic_notification');
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings);
   }
 }
